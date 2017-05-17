b9aa2cfd492b9a0e92a70f0b2fb633f2d2434b1c
salvarosales <qognos@gmail.com>
2017-05-17 01:24:00 -0600

    Update IAYoutube_converter.php

--- IAYoutube_converter.php
+++ IAYoutube_converter.php
@@ -63,7 +63,7 @@ $totalRows_Linker = mysql_num_rows($Linker);
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Random Number</title>
+<title>Instant Articles Converter</title>
 <style type="text/css">
 body {
 	background-color: #000;
